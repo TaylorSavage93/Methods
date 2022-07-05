c2291eeac6abf8d5703c615c2641cfeb6b1a3859
Taylor Savage <taylorsavage93@gmail.com>
2022-07-05 11:21:13 -0400

    created methods exercise 1

---
+++ Methods Exercise.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Name: Taylor
+// Favorite Color: Blue
+//Favorite Animal: Panda
+//Favortie Band: Amity affliction
+
+Console.WriteLine("Hello - What is your first name?");
+var username = Console.ReadLine();
+
+Console.WriteLine($"Hi, {username} What is your facvorite color?");
+var color = Console.ReadLine();
+
+Console.WriteLine($"{color} is also my favorite. what is your favorite animal?");
+var animal = Console.ReadLine();
+
+Console.WriteLine($"Thats interesting the {animal} is very cut. What is your favorite band?");
+var band = Console.ReadLine();
+
+
+
+
+
